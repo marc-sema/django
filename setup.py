30be6e6d5a821d3d4e9a9092610f406684ebc2aa
marc-sema <142593415+marc-sema@users.noreply.github.com>
2024-04-30 23:28:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from distutils.sysconfig import get_python_lib
 
 from setuptools import setup
 
-
 # Allow editable install into user site directory.
 # See https://github.com/pypa/pip/issues/7953.
 site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
