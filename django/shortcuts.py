5688c421bee1e76d8221d14d81d1bc69a5aa8792
marc-sema <142593415+marc-sema@users.noreply.github.com>
2024-05-07 19:49:13 +0300

    Update shortcuts.py

--- django/shortcuts.py
+++ django/shortcuts.py
@@ -10,9 +10,6 @@ from django.http import (
     HttpResponseRedirect,
 )
 from django.template import loader
-
-
-
 from django.urls import NoReverseMatch, reverse
 from django.utils.functional import Promise
 
